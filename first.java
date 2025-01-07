df2ae0c40e6d7f74d2d2898fb8325e8a2281f53a
vinay_jadaun <vinay11112003@gmail.com>
2025-01-07 13:57:53 +0530

    first commit on vinay branch

--- first.java
+++ first.java
@@ -2,5 +2,8 @@ class first {
     public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("WELCOME TO THE FILES SET OF CAPGEMINI TRAINING CREATED AT 07-01-2025");
+        for(int i=0;i<5;i++){
+            System.out.println("hello vinay");
+        }
     }
 }
