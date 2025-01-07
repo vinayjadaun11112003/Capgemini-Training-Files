e0132d036e1d33104287ad54ad8c81badec2c3e9
vinay_jadaun <vinay11112003@gmail.com>
2025-01-07 13:34:36 +0530

    second

--- first.java
+++ first.java
@@ -1,5 +1,6 @@
 class first {
     public static void main(String[] args){
         System.out.println("Hello world");
+        System.out.println("WELCOME TO THE FILES SET OF CAPGEMINI TRAINING CREATED AT 07-01-2025");
     }
 }
